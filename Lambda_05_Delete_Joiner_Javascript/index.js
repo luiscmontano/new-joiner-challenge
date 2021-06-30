4979cfc6ce2b48de45b74fc7285b8d6391f5ccb2
luiscmontano <luismontano@outlook.com>
2021-06-30 15:14:12 -0500

    Create index.js

---
+++ Lambda_05_Delete_Joiner_Javascript/index.js
@@ -0,0 +1,22 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+
+    //console.log("Joiner: " + JSON.stringify(event, null, 4));
+    console.log(" ------ Delete Joiner [START]:  " + new Date().toLocaleTimeString());
+    
+    var id = parseInt(event.pathParameters.id, 10);
+    
+    await dynamo.delete({
+        TableName: "Joiner",
+        Key: {
+            identificationNumber: id
+        }
+
+    }).promise();
+    
+    console.log(" ------ Delete Joiner [END]:    " + new Date().toLocaleTimeString());
+    
+    return event.pathParameters.id;
+};
