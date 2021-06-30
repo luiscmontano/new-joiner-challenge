24e50ec51c3a6f0f1089734886688e66e69f8a2e
luiscmontano <luismontano@outlook.com>
2021-06-30 11:58:12 -0500

    Create Joiner

---
+++ Lambda_03_Create_Joiner_Javascript/index.js
@@ -0,0 +1,22 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+
+    console.log(" ------ Process Document [START]:  " + new Date().toLocaleTimeString());
+    
+    var body = event.Records ? JSON.parse(event.Records[0].body) : event.body;
+    var joiner = JSON.parse(body);
+    
+    joiner.id = new Date().getTime();
+    await dynamo.put({
+        TableName: "Joiner",
+        Item: joiner
+    }).promise();
+    
+    console.log("Joiner: " + JSON.stringify(joiner, null, 4));
+    
+    console.log(" ------ Process Document [END]:    " + new Date().toLocaleTimeString());
+    
+    return joiner;
+};
